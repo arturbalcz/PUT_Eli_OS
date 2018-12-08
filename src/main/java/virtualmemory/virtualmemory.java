f543e350da6900c0205a21c81279749eeef0d451
Grachu <grachu9099@gmail.com>
2018-12-08 18:11:39 +0100

    Push just for sending file

--- src/main/java/virtualmemory/virtualmemory.java
+++ src/main/java/virtualmemory/virtualmemory.java
@@ -1,34 +1,59 @@
-package virtualmemory;
+﻿package virtualmemory;
 
 import java.util.Map;
 import java.util.Queue;
 import java.util.Vector;
 import java.util.function.Function;
 
-class Page{
-    Integer pageId, processId;
-    //Virtual address
-    //Physical address
-    //Rozmiar???
-}
-class PageTable{
-
-}
 class Process{
     Integer processId;
-    Vector<Page> processVector;
+    Vector<virtualmemory.Page> processVector;
 }
 public class virtualmemory {
-    boolean freeFrames;
-    //Queue musi mieć rozmiar rzeczywistej liości ramek w pamięci fizycznej, albo po prostu będzie licznik, który nie pozwoli na przejśćie
-    Queue <Page> victimQueue;
-    // Ten integer w mapie to id procesu
-    Map<Integer, Process> PageFile;
-    //Funkcja wywoływana przez Szymona, który będzie dzielił kod na Strony
-    void putProcessToPageFile(Process proc){
-        PageFile.put(proc.processId, proc);
+    public
+    class Page{
+        boolean valid;
+        Integer frame;
+        Page(Boolean v, Integer f){
+            valid = v;
+            frame = f;
+        }
+    }
+    void getProcess(Process p){
+        processSpliter(p);
+        putProcessToPageFile(p, program);
+
+    }
+    private
+    char[] bajt = new char [8];
+    Vector <char[]> chunk;
+    Vector<Vector <char[]>> program;
+
+    void processSpliter(Process p){
+        int size = p.processVector.size(); //size of program code
+        while (size != 0){
+            for(int i=0; i<16; i++){
+                for(int j=0; j<8; j++){
+                bajt[j]= 1; //Tutaj przypisuję każdy kolejny znak do tablicy charów i następnie wpisuję je do vectora zbiorczego
+                }
+                chunk.add(bajt);
+            }
+            program.add(chunk);
+        }
     }
 
+
+    //Queue musi mieć rozmiar rzeczywistej liości ramek w pamięci fizycznej, albo po prostu będzie licznik, który nie pozwoli na przejśćie
+    Queue <Page> victimQueue;
+
+    // Ten integer w mapie to id procesu
+    Map<Integer, Vector<Vector <char[]>>> PageFile;
+
+
+    void putProcessToPageFile(Process proc, Vector<Vector <char[]>> prog){
+        PageFile.put(proc.processId, prog);
+    }
+    void removeProcessFormPageFile(Process proc) { PageFile.remove(proc.processId, proc);}
     void pageSwap(Integer iTF, Integer prId){
         Page adin = findVictim();
         Page dwa = findSpecificPage(iTF, prId);
@@ -36,6 +61,7 @@ public class virtualmemory {
     void pageOut(){
         //Funkcja, która zabiera ofiarę z ramu do pliku stronicowania
     }
+
     void pageIn(){
         //Funkcja, która wprowadza stronicę do ramu z pliku stronicowania
     }
