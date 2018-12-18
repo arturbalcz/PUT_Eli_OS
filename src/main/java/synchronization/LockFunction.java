a30d969a577586fc897e0805026a82dac4abe121
jakubriegel <jakubriegel@hotmail.com>
2018-12-18 22:14:01 +0100

    tests for LockFunction

--- src/main/java/synchronization/LockFunction.java
+++ src/main/java/synchronization/LockFunction.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 import processess.ProcessState;
 
-public class Lock_function {
-    //initilize lock which is open
-    private Boolean lock;
+public class LockFunction {
+    //initilize locked which is open
+    private boolean locked;
    // PCB pcb = new PCB();
     private List<processess.PCB> kolejka = new LinkedList<processess.PCB>();
     //initialize queue FIFO;
@@ -18,36 +18,36 @@ public class Lock_function {
     Object fileName;
 
     //it can be usfull we will see(never enough variable :D )
-    Lock_function(Object fileName) {
+    LockFunction(Object fileName) {
         this.fileName = fileName;
-        this.lock = false;
+        this.locked = false;
     }
 
     void lock(PCB proces) {
-        if (lock == true) {
+        if (locked == true) {
             proces.setState(ProcessState.WAITING);
             System.out.println("Ktos juz uzywa ten plik. " + name + "musi czekac");
             kolejka.add(proces);
         } else {
             System.out.println("LOCK zostal zablokowany " + name);
-            lock = true;
+            locked = true;
             name = name;
             //this function take the lock
             //procces is been stopped
         }
     }
 
-    void unlock(boolean lockers) {
+    void unlock(boolean lockers) { // TODO co to jest lockers?
         System.out.println(name + " Zakonczyl swoje zadanie");
-        this.lock = false;
+        this.locked = false;
         if (kolejka.size() != 0) {
-            System.out.println("Kolejka lock jest pusta");
+            System.out.println("Kolejka locked jest pusta");
             //Procces which is in quer will be start
         } else {
             PCB process = kolejka.get(0);
             System.out.println("Process" + process.name + "rozpoaczac swoja akcje");
         }
-        //reclaim the lock
+        //reclaim the locked
         //procces is been activate from the stopped
     }
 
@@ -64,7 +64,7 @@ public class Lock_function {
          }
         else
          {
-            lock=true;
+            locked =true;
          }
 
      }
--- src/test/groovy/synchronization/LockFunctionSpecification.groovy
+++ src/test/groovy/synchronization/LockFunctionSpecification.groovy
@@ -1,4 +1,82 @@
 package synchronization
 
-class LockFunctionSpecification {
+import processess.PCB
+import processess.ProcessState
+import spock.lang.Specification
+
+class LockFunctionSpecification extends Specification {
+    def lockFunction = new LockFunction()
+
+    PCB p1 = Mock()
+    PCB p2 = Mock()
+
+    def setup() {
+        p1.PID >> 1
+        p1.name >> "p1"
+
+        p1.PID >> 2
+        p1.name >> "p2"
+    }
+
+    def "should lock" () {
+        when:
+        lockFunction.lock(p1)
+
+        then:
+        lockFunction.locked
+        0 * p1.setState(_)
+        lockFunction.kolejka == []
+    }
+
+    def "should lock for specified process" () {
+        when:
+        lockFunction.lock(p1)
+        lockFunction.lock(p2)
+
+        then:
+        lockFunction.locked
+        1 * p2.setState(ProcessState.WAITING)
+        lockFunction.kolejka == [p2]
+    }
+
+    def "should unlock" () {
+        given:
+        lockFunction.locked = true
+
+        when:
+        lockFunction.unlock(true)
+
+        then:
+        !lockFunction.locked
+        0 * p1.setState(_)
+        lockFunction.kolejka == []
+    }
+
+    def "should unlock and lock for waiting process" () {
+        given:
+        lockFunction.locked = true
+        lockFunction.kolejka = [p2]
+
+        when:
+        lockFunction.unlock(true)
+
+        then:
+        lockFunction.locked
+        0 * p1.setState(_)
+        1 * p2.setState(ProcessState.READY)
+        lockFunction.kolejka == []
+    }
+
+    def "should signal that process is ready for execution" () {
+        given:
+        lockFunction.kolejka = [p1]
+
+        when:
+        lockFunction.signal()
+
+        then:
+        1 * p1.setState(ProcessState.READY)
+        lockFunction.kolejka == []
+    }
+
 }
