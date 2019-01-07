fc2c1341ce2e3d43c8f55c190bd0882828a3029d
R0ckwe11 <r0ckwe112233@gmail.com>
2019-01-07 07:06:03 +0100

    Quick fix for data writing (line 21); next problem lays in virtualmemory (check comments)

--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -18,7 +18,7 @@ public class Memory {
             return false;
         }
 
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < data.size(); i++) {
             memory[frame * 16 + i] = data.get(i);
         }
 
