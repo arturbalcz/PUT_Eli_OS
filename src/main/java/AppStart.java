37456f209ac835de64c36bbe0818a37c9cf2f2c7
Maciej S <starwars.maciek@gmail.com>
2019-01-08 11:12:51 +0100

    Added: Bluescreen for unhandled exceptions

--- src/main/java/AppStart.java
+++ src/main/java/AppStart.java
@@ -1,6 +1,9 @@
 import os.OS;
 import utils.Utils;
 
+import java.io.IOException;
+import java.sql.SQLOutput;
+
 /**
  * Entry point of system
  */
@@ -8,9 +11,37 @@ public class AppStart {
     public static void main(String[] args) {
         Utils.log("OS starting...");
 
-        OS os = new OS();
-        os.run();
-
+        try {
+            OS os = new OS();
+            os.run();
+        }
+        catch(Exception e){
+            try {
+                new ProcessBuilder("cmd", "/c", "color 17").inheritIO().start().waitFor();
+                for (int i = 0; i < 20; i++) {
+                    System.out.println();
+                }
+                String msg = "\t\t\tPUT Elita OS \n" +
+                        "\n" +
+                        "\tA fatal exception " + e.hashCode() + " has occured. \n" +
+                        "\tThe current application will now be terminated. \n" +
+                        "\n";
+                System.out.println(msg);
+                e.printStackTrace();
+                System.out.println();
+                System.out.println("\tPress return key to continue ");
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.in.read();
+                new ProcessBuilder("cmd", "/c", "color 07").inheritIO().start().waitFor();
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+        }
         //Utils.stepOn();
         Utils.step("quitting...");
         Utils.closeLogs();
